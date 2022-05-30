c475148e68d63e539889c76fe6526461f721fb23
Kevin Harrington <mad.hephaestus@gmail.com>
2022-05-30 13:57:30 -0400

    Windows testing

--- src/main/java/mujoco/java/MuJoCoConfig.java
+++ src/main/java/mujoco/java/MuJoCoConfig.java
@@ -7,6 +7,7 @@ import org.bytedeco.javacpp.tools.*;
 
 @Properties(
     value = @Platform(
+    	value = {"linux-x86_64", "macosx", "windows_64"}, 
         includepath = {"/tmp/mujoco/include/"},
         linkpath = {"/tmp/mujoco/lib/"},
         include = {

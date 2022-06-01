7a9f49c1d8276ccf79ddfa525bb778c0a3e94731
Kevin Harrington <mad.hephaestus@gmail.com>
2022-06-01 11:03:17 -0400

    Updating publish

--- publish.sh
+++ publish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cat <(echo -e "$OSSRH_GPG_SECRET_KEY") | gpg --batch --import
+cat <(echo -e $OSSRH_GPG_SECRET_KEY) | gpg --batch --import
 gpg --list-secret-keys --keyid-format LONG
+
+ls -al $HOME/.gnupg/
 
 ./gradlew publish -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=$OSSRH_GPG_SECRET_KEY_ID

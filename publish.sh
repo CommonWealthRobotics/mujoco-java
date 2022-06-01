bcda5de685c37a84d077e436916d66bbf3b970af
Kevin Harrington <mad.hephaestus@gmail.com>
2022-06-01 13:42:42 -0400

    updating the jar copy

--- publish.sh
+++ publish.sh
@@ -6,4 +6,8 @@ gpg1 --list-secret-keys --keyid-format LONG
 ls -al $HOME/.gnupg/
 
 ./gradlew publish -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=$OSSRH_GPG_SECRET_KEY_ID
+# copy the jar up for release
+cp  build/libs/mujoco-java-$VERSION_SEMVER.jar  mujoco-java.jar
+# close and release Sonatype
 ./gradlew closeAndReleaseRepository -PnexusUsername=$MAVEN_USERNAME -PnexusPassword=$MAVEN_PASSWORD
+

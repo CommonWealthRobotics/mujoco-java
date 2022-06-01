40f9e8ded398aac51bfe285b37abe9655ed89c33
Kevin Harrington <mad.hephaestus@gmail.com>
2022-06-01 13:23:03 -0400

    attempting to publish fully from CI

--- publish.sh
+++ publish.sh
@@ -6,3 +6,4 @@ gpg1 --list-secret-keys --keyid-format LONG
 ls -al $HOME/.gnupg/
 
 ./gradlew publish -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=$OSSRH_GPG_SECRET_KEY_ID
+./gradlew closeAndReleaseRepository -PnexusUsername=$MAVEN_USERNAME -PnexusPassword=$MAVEN_PASSWORD

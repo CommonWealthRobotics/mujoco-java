9f96c7228f87ea2917a3fcd58162534f0b124e1b
Kevin Harrington <kharrington@softroboticsinc.com>
2022-06-02 09:49:51 -0400

    Win update

--- src/main/java/mujoco/java/Search.java
+++ src/main/java/mujoco/java/Search.java
@@ -6,18 +6,25 @@ public class Search {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		search(new File("C:/"));
+		search(new File("D:/"),0);
+		search(new File("C:/"),0);
 	}
 	
-	private static void search(File f) {
+	private static void search(File f,int depth) {
+		if(depth<5)
+			System.out.println("\tSearching "+f.getAbsolutePath());
+		else {
+			// bailing
+			return;
+		}
 		if(f.isDirectory()) {
 			for(File f1:f.listFiles()) {
-				search(f1);
+				search(f1,depth+1);
 			}
 		}
 		else {
 			if(f.getName().contains("mujoco"))
-				System.out.println(f.getAbsolutePath());
+				System.out.println("FOUND!= "+f.getAbsolutePath());
 		}
 	}
 

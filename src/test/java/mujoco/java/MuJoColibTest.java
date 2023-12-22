60f51196a7de627871895fe1a2ae3c6020723c04
Kevin Harrington <mad.hephaestus@gmail.com>
2023-12-22 16:34:11 -0500

    Adding additional example code

--- src/test/java/mujoco/java/MuJoColibTest.java
+++ src/test/java/mujoco/java/MuJoColibTest.java
@@ -5,6 +5,11 @@ package mujoco.java;
 
 import org.junit.Test;
 import org.mujoco.MuJoCoLib;
+import org.mujoco.MuJoCoLib.mjData;
+import org.mujoco.MuJoCoLib.mjData_;
+import org.mujoco.MuJoCoLib.mjModel;
+import org.mujoco.MuJoCoLib.mjModel_;
+import org.mujoco.MuJoCoLib.mjVFS;
 
 import static org.junit.Assert.*;
 
@@ -12,13 +17,42 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.annotation.Cast;
+import org.bytedeco.javacpp.annotation.Const;
 
 public class MuJoColibTest {
-    @Test public void mujocoJNILoadTest() {
-    	System.out.println(System.getProperty("org.bytedeco.javacpp.logger.debug"));
-    	System.setProperty("org.bytedeco.javacpp.logger.debug", "true");
-    	MuJoCoLib lib = new MuJoCoLib();
-        
-        System.out.println("Starting "+MuJoCoLib.mj_versionString().getString());
-    }
+	@Test
+	public void mujocoJNILoadTest() {
+		System.out.println(System.getProperty("org.bytedeco.javacpp.logger.debug"));
+		System.setProperty("org.bytedeco.javacpp.logger.debug", "true");
+		MuJoCoLib lib = new MuJoCoLib();
+
+		System.out.println("Starting " + MuJoCoLib.mj_versionString().getString());
+		byte[] error = new byte[100];
+		int error_sz = 0;
+		mjModel m = MuJoCoLib.mj_loadXML(
+				"/home/hephaestus/git/mujoco-java/src/main/resources/mujoco/java/humanoid/humanoid.xml", null, error,
+				error_sz);
+		System.out.println("Humanoid model loaded " + m);
+		mjData d = MuJoCoLib.mj_makeData(m);
+		try {
+			mjModel_ Maccessable = new mjModel_(m);
+			try (mjData_ accessable = new mjData_(d)) {
+				System.out.println("Run model for 10 seconds");
+				while (accessable.time() < 10) {
+					MuJoCoLib.mj_step(m, d);
+					Thread.sleep(1);
+					
+				}
+
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Clean up data objects");
+
+		MuJoCoLib.mj_deleteData(d);
+		MuJoCoLib.mj_deleteModel(m);
+	}
 }

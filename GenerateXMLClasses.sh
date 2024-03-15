3f364050c02a23fa111a7a81f1fc5bbd89a2af1f
Kevin Harrington <mad.hephaestus@gmail.com>
2024-03-15 19:44:21 -0400

    Auto-cleanup of the generated code to adhere to the version control

--- GenerateXMLClasses.sh
+++ GenerateXMLClasses.sh
@@ -6,7 +6,7 @@
 
 #cd ../
 
-#mvn generate-sources
+mvn generate-sources
 
 #echo "Error! the generated sources needed extensive hand edits to work correctly!";return 1
 
@@ -17,6 +17,7 @@ FILES=$(grep -r -l "// Generated on: " src/main/java/org/mujoco/xml/)
 for VARIABLE in $FILES
 do
 	sed -i '/Generated on\:/d' $VARIABLE
+	sed -i -e 's/{@link PropertyPath}//g'  $VARIABLE
 done
 
 #./gradlew javadoc
